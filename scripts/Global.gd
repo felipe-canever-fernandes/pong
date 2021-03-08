c48e3ad92cb65316a7e302cf50f1e726faf2f497
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-03-08 20:07:34 -0300

    Detect system locale

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -5,6 +5,10 @@ var version : String
 var author : String
 
 func _ready() -> void:
+	_load_game_info()
+	TranslationServer.set_locale(OS.get_locale())
+
+func _load_game_info() -> void:
 	var export_config = ConfigFile.new()
 	export_config.load("res://export_presets.cfg")
 	
