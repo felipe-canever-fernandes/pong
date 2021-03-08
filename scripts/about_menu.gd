d31449b0ed36ec129e97beee2fa464cb64568a44
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-03-08 19:43:11 -0300

    Set about description programmatically

--- scripts/about_menu.gd
+++ scripts/about_menu.gd
@@ -2,5 +2,10 @@ extends Control
 
 export(String, FILE, "*.tscn") var main_menu : String = ""
 
+onready var about_label : Label = $CenterContainer/VBoxContainer/AboutLabel
+
+func _ready() -> void:
+	about_label.text = "PONG Alpha 1.0.0\n\nby\nFelipe Canever Fernandes\n\nusing\nGodot Engine\n"
+
 func _on_MainMenuButton_pressed():
 	get_tree().change_scene(main_menu)
