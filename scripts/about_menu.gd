09be3f570ac1c679a97653fc09baf7229192c70f
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-03-08 20:01:00 -0300

    Load about description from translation file

--- scripts/about_menu.gd
+++ scripts/about_menu.gd
@@ -5,12 +5,11 @@ export(String, FILE, "*.tscn") var main_menu : String = ""
 onready var about_label : Label = $CenterContainer/VBoxContainer/AboutLabel
 
 func _ready() -> void:
-	about_label.text = "{title} {version}\n\nby\n{author}\n\nusing\nGodot Engine\n"\
-		.format({
-			"title": Global.title,
-			"version": Global.version,
-			"author": Global.author
-		})
+	about_label.text = (tr("about_text") + "\n").format({
+		"title": Global.title,
+		"version": Global.version,
+		"author": Global.author
+	})
 
 func _on_MainMenuButton_pressed():
 	get_tree().change_scene(main_menu)
